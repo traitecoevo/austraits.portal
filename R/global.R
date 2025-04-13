9c052f6213cbd1b4ca9e90f84f37fa2cd9d40713
Fonti Kar <f.kar@unsw.edu.au>
2025-04-14 09:59:58 +1000

    Reverting to working state

--- R/global.R
+++ R/global.R
@@ -1,37 +1,9 @@
 # Load data
 ## TODO: One day parquet of flattened database may be uploaded to Zenodo,
-## For now will use the R package and store in Github Releases see data-raw/create-flat-austraits.R
-
-load_parquet <- function(dir = system.file("extdata/austraits", package = "austraits.portal")) {
-  # Check if the directory exists
-  if (!dir.exists(dir)) {
-    stop("Directory does not exist: ", dir)
-  } 
-
-  # Get latest version
- get_latest_version <- function(dir = system.file("extdata/austraits", package = "austraits.portal")){ 
-  if(length(list.files(dir)) == 0){
-    stop("No files found in the directory: ", dir)
-  }
-
-  grep("[0-9]+\\.[0-9]+\\.[0-9]+", list.files(dir), value = TRUE) |>
-    gsub(pattern = "austraits-|\\-flatten.parquet", replacement = "")  |> 
-    numeric_version() |>
-    sort(decreasing = TRUE)  |> 
-    dplyr::first()  |> 
-    as.character()
-  }
-
-  # Load the first parquet file
-  dataset <- arrow::open_dataset(paste0(dir, "/austraits-", get_latest_version(dir), "-flatten.parquet"))
-          # arrow::open_dataset("inst/extdata/austraits/austraits-6.0.0-flatten.parquet") 
-          # arrow::open_dataset("inst/extdata/austraits/austraits-lite.parquet") 
-  
-  return(dataset)
-}
+## For now will use the R package and store in Github Releases see branch data-load
 
 # Load the austraits dataset
-austraits <- load_parquet()
+austraits <- arrow::open_dataset("inst/extdata/austraits/austraits-lite.parquet")
 
 # Set up possible values for selectize menus
 
