e18b6c0779f71f2333697c4d4e007a40ca2a97bf
Daniel Falster <daniel.falster@unsw.edu.au>
2025-05-06 08:11:16 +1000

    Rework apply_filters (#11)

--- R/a-helper.R
+++ R/a-helper.R
@@ -10,33 +10,38 @@ has_input_value <- function(input, input_name) {
 
 
 valid_filters <- function(input, exclude_taxon_rank = TRUE){
-  valid_vars <- c(names(austraits))
   
-  if(exclude_taxon_rank){
-    valid_vars <- valid_vars[valid_vars != "taxon_rank"]
-    stringr::str_subset(names(input), paste(valid_vars, collapse = "|"))
-  } else
-  stringr::str_subset(names(input), paste(valid_vars, collapse = "|"))
+  # Get the names of the input variables
+  v <- names(input)
+  # Limit to values in the database
+  v <- v[v %in% names(austraits)]
+  # remove null values
+  v <- v[purrr::map_lgl(v, ~ !is.null(input[[.x]]))]
+  # remove taxon rank if requested
+  if (exclude_taxon_rank) {
+    v <- v[v != "taxon_rank"]
+  }
+
+  v
 }
 
 #' Apply filters
 #' @keywords internal
 apply_filters <- function(data = austraits, input){
   
-  # Exclude user inputs that we can't filter on
+  # Generate a list of filter conditions based on the input
   # TODO need to exclude data_table_ prefixes too if column filters enabled
-  valid_filters <- valid_filters(input)
+  filter_conditions <-
+    input |>
+    valid_filters() |>
+    # Construct filter conditions dynamically
+    purrr::map(function(v) {
+      # paste in case value contains more than one item
+      value <- input[[v]] |> paste(collapse = "|") 
+      # formulate the filter condition as an expression
+      expr(stringr::str_detect(.data[[v]], !!value))
+    })
   
-  # Construct filter conditions dynamically
-  filter_conditions <- purrr::map(valid_filters, function(v) {
-    value <- input[[v]] |> paste(collapse = "|")   # paste in case value contains more than one item
-    if (!is.null(value)) {
-      expr(stringr::str_detect(.data[[v]], !!value))  # Dynamically create filter expressions
-    } else {
-      NULL
-    }
-  }) |> purrr::compact()  # Remove NULL conditions
-
   # Combine all filter conditions into a single filter call
   filtered_parquet <- data |> 
     dplyr::filter(!!!filter_conditions)  # Unquote and splice the conditions
