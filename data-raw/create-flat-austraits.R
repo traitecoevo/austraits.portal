b6d75955be1664bc9fe80847ba3110df5e8fccf3
Daniel Falster <daniel.falster@unsw.edu.au>
2025-05-12 16:48:56 +1000

    Code refactoring

--- data-raw/create-flat-austraits.R
+++ data-raw/create-flat-austraits.R
@@ -15,3 +15,20 @@ write_parquet(flatten_austraits, "data/austraits/austraits-6.0.0-flatten.parquet
 
 ## Save lite version as a parquet
 write_parquet(austraits:::austraits_5.0.0_lite |> flatten_database(), "data/austraits/austraits-lite.parquet")
+
+## Create species means
+
+austraits <- arrow::read_parquet("inst/extdata/austraits/austraits-lite.parquet")
+
+# Numeric traits
+traits <- c("leaf_area", "leaf_mass_per_area", "leaf_nitrogen_content", "leaf_phosphorus_content", "leaf_carbon_content", "leaf_silicon_content", "leaf_stable_isotope_ratio")
+
+x <- austraits_weighted_means(austraits, traits)
+
+# Categorical traits
+traits <- c("serotiny", "dispersal_syndrome", "clonal_spread_mechanism", "bud_bank_location")
+
+
+categorical_trait_summary(austraits, traits)
+
+categorical_trait_value_summary(austraits, traits)
