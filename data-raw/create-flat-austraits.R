86a1c823ec42dc225d7722c4fc3dedbfc90ca6ac
Eve Miles <eve.miles09@gmail.com>
2025-05-20 13:39:14 +1000

    data generator now generates full dataset

--- data-raw/create-flat-austraits.R
+++ data-raw/create-flat-austraits.R
@@ -4,30 +4,34 @@ library(dplyr)
 
 #------------------------------- HELPER FUNCTIONS ------------------------------
 # Truncate data helper
-austraits_truncate <- function(formatted_data, original_data) {
-  text_columns <- sapply(original_data, is.character)
-  columns_to_exclude <- c("taxon_name", "trait_name", "genus", "family", "source_primary_citation")  # Exclude the hyperlink column from truncation
-  for (col in names(original_data)[text_columns]) {
+austraits_truncate <- function(data) {
+  text_columns <- sapply(data, is.character)
+  columns_to_exclude <- c("taxon_name", "trait_name", "genus", "family", "source_primary_citation")
+  for (col in names(data)[text_columns]) {
     # Skip the excluded columns
     if (col %in% columns_to_exclude) next
     
-    formatted_data[[col]] <- sapply(original_data[[col]], function(x) {
+    data[[col]] <- sapply(data[[col]], function(x) {
       if (is.na(x) || is.null(x)) return(x)
       if (nchar(x) > 20) {
         paste0(substr(x, 1, 20), "...")
       } else {
-        x
+        return(x)
       }
     })
   }
-  return(formatted_data)
+  return(data)
 }
 
 #------------------------------------------------------------
 # Save lite version as a parquet
-austraits:::austraits_5.0.0_lite |> flatten_database() |>
-    mutate(row_id = row_number())|>
-write_parquet("inst/extdata/austraits/austraits-lite-obs.parquet")
+# austraits:::austraits_5.0.0_lite |> 
+#   flatten_database() |>
+#   mutate(row_id = row_number()) |>
+#   format_database_for_display() |> 
+#   format_hyperlinks_for_display() |>
+#   austraits_truncate() |>
+#   write_parquet("inst/extdata/austraits/austraits-lite-display-obs.parquet")
 
 #------------------------------------------------------------
 # Save the full database as a parquet
@@ -40,59 +44,58 @@ austraits <- load_austraits(version = "6.0.0", path = "inst/extdata/austraits", 
 austraits$definitions |> yaml::write_yaml("inst//extdata/austraits/definitions.yml")
 austraits$sources |> RefManageR::WriteBib("inst/extdata/austraits/sources.bib")
 
-print("Formatting for use")
-austraits <- austraits |> 
+# Flatten the database
+austraits_flat <- austraits |> 
   flatten_database() |> 
   mutate(row_id = row_number()) |> 
-    mutate(measurement_remarks = iconv(measurement_remarks,
-      from = "",      # Let R guess the current encoding
-      to = "UTF-8",   # Convert to UTF-8
-      sub = ""        # Remove any bytes that can't be converted
-    ))
-print("done")
+  mutate(measurement_remarks = iconv(measurement_remarks,
+    from = "",      # Let R guess the current encoding
+    to = "UTF-8",   # Convert to UTF-8
+    sub = ""        # Remove any bytes that can't be converted
+  ))
+
+print("Formatting for use")
+austraits_display <- austraits_flat
 
 # Database for DT display
 print("Formatting for display")
-austraits_display <- austraits |> 
+austraits_display <- austraits_display |> 
   format_database_for_display() |> 
   format_hyperlinks_for_display()
 print("done")
 
 # Truncate display db
 print("Truncate db")
-austraits_display <- austraits_truncate(austraits_display, austraits)
+austraits_display <- austraits_truncate(austraits_display)
 print("done")
 
-## Flatten and format the databases
-# flatten_austraits <- austraits_format(austraits)
-
 ## Save the flattened and display database
-# write_parquet(flatten_austraits, "inst/extdata/austraits/austraits-6.0.0-flatten.parquet")
+write_parquet(austraits_flat, "inst/extdata/austraits/austraits-6.0.0-flatten.parquet")
 write_parquet(austraits_display, "inst/extdata/austraits/austraits-6.0.0-flatten-display.parquet")
 
 #------------------------------------------------------------
 # Save an intermediate sized version, to reduce size of the database
 # Only has a subset of taxa and only core traits
 
-## Only core traits
-traits <- read_csv_arrow("inst/extdata/austraits/trait_groups_for_portal.csv") |> filter(!is.na(core_trait)) |> pull(trait)
+# ## Only core traits
+# traits <- read_csv_arrow("inst/extdata/austraits/trait_groups_for_portal.csv") |> filter(!is.na(core_trait)) |> pull(trait)
 
-## Choose random selectioon of taxa, plus those in lite version
-set.seed(123)
-taxon_names <- c(
-    flatten_austraits$taxon_name |> unique() |> sample(5000),
-    austraits:::austraits_5.0.0_lite$traits$taxon_name |> unique()
-    ) |> sort()
+# ## Choose random selectioon of taxa, plus those in lite version
+# set.seed(123)
+# taxon_names <- c(
+#     flatten_austraits$taxon_name |> unique() |> sample(5000),
+#     austraits:::austraits_5.0.0_lite$traits$taxon_name |> unique()
+#     ) |> sort()
 
-## filter and save
-flatten_mid <- 
-    flatten_austraits |>
-    filter(taxon_name %in% taxon_names, trait_name %in% traits) |>
-    mutate(row_id = row_number())
-write_parquet(flatten_mid, "inst/extdata/austraits/austraits-6.0.0-mid-flatten.parquet")
+# ## filter and save
+# flatten_mid <- 
+#     flatten_austraits |>
+#     filter(taxon_name %in% taxon_names, trait_name %in% traits) |>
+#     mutate(row_id = row_number())
+# write_parquet(flatten_mid, "inst/extdata/austraits/austraits-6.0.0-mid-flatten.parquet")
 
-## species averages
-flatten_mid_avg <- 
-    flatten_mid |> estimate_species_trait_means()
+# ## species averages
+# flatten_mid_avg <- 
+#     flatten_mid |> estimate_species_trait_means()
 
-write_parquet(flatten_mid_avg, "inst/extdata/austraits/austraits-6.0.0-mid-flatten-means.parquet")
+# write_parquet(flatten_mid_avg, "inst/extdata/austraits/austraits-6.0.0-mid-flatten-means.parquet")
