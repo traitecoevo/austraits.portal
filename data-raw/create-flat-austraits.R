d5831bffff97e06ab8f00276f1b624b9af4527a0
Fonti Kar <f.kar@unsw.edu.au>
2025-05-26 08:41:20 +1000

    Updating naming convention for parquets

--- data-raw/create-flat-austraits.R
+++ data-raw/create-flat-austraits.R
@@ -2,49 +2,58 @@ library(austraits)
 library(arrow)
 library(dplyr)
 
-#TODO: Use a consistent naming convention, the 'obs' doesn't add much info about what the parquet is
-# My suggestion is to use 'austraits-lite.parquet' 
+# TODO: Encoding issue in full database
 
 # LITE VERSION
 #------------------------------------------------------------
 # Save lite version as a parquet
-austraits:::austraits_5.0.0_lite |> 
+austraits_lite_flatten <- austraits:::austraits_5.0.0_lite |> 
     austraits::flatten_database() |>
-    dplyr::mutate(row_id = dplyr::row_number())|>
-    arrow::write_parquet("inst/extdata/austraits/austraits-lite-obs.parquet")  
+    dplyr::mutate(row_id = dplyr::row_number())
+
+austraits_lite_flatten |> 
+    arrow::write_parquet("inst/extdata/austraits/austraits-lite-flatten.parquet")  
 
 # Save the display version of the lite database
-
-austraits:::austraits_5.0.0_lite |> 
-    austraits::flatten_database() |>
-    dplyr::mutate(row_id = dplyr::row_number())|>
+austraits_lite_flatten
     format_database_for_display() |> 
     format_hyperlinks_for_display() |> 
     arrow::write_parquet("inst/extdata/austraits/austraits-lite-display.parquet")
 #------------------------------------------------------------
-
 # FULL VERSION
+#------------------------------------------------------------
 # Save the full database as a parquet
 
 ## Load data
 ## TODO: One day parquet of flattened database may be uploaded to Zenodo, needs row_id added to Zenodo upload
 ## For now will use the R package and store in Github Releases
-austraits <- load_austraits(version = "6.0.0", path = "inst/extdata/austraits", update = FALSE)
+austraits <- austraits::load_austraits(version = "6.0.0", path = "inst/extdata/austraits", update = FALSE)
+
+# Saving definitions and sources for use in the portal
 austraits$definitions |> yaml::write_yaml("inst//extdata/austraits/definitions.yml")
 austraits$sources |> RefManageR::WriteBib("inst/extdata/austraits/sources.bib")
 
 ## Flatten the database
-flatten_austraits <- austraits |> flatten_database()  |> 
-    mutate(row_id = row_number())  |> 
-    mutate(measurement_remarks = iconv(measurement_remarks,
-                    from = "",      # Let R guess the current encoding
+austraits_full_flatten <- 
+    austraits |> 
+    austraits::flatten_database()  |> 
+    dplyr::mutate(row_id = dplyr::row_number())  |> 
+    dplyr::mutate(measurement_remarks = iconv(measurement_remarks,
+                    from = "",      # Let's R guess the current encoding
                     to = "UTF-8",   # Convert to UTF-8
                     sub = ""        # Remove any bytes that can't be converted
     ))
 
 ## Save the flattened database
-write_parquet(flatten_austraits, "inst/extdata/austraits/austraits-6.0.0-flatten.parquet")
+arrow::write_parquet(austraits_full_flatten, "inst/extdata/austraits/austraits-6.0.0-flatten.parquet")
 
+# Save the display version of the full  database
+austraits_full_flatten
+    format_database_for_display() |> 
+    format_hyperlinks_for_display() |> 
+    arrow::write_parquet("inst/extdata/austraits/austraits-6.0.0-display.parquet")
+#------------------------------------------------------------
+# MID VERSION
 #------------------------------------------------------------
 # Save an intermediate sized version, to reduce size of the database
 # Only has a subset of taxa and only core traits
